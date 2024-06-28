5f6a6c8d7925660d52736b5c29a6d9c70fb93795
MrBejvan <bejvaben@gmail.com>
2024-06-27 20:59:40 -0600

    Create euclidsalg.py

---
+++ euclidsalg.py
@@ -0,0 +1,21 @@
+def EU (a, b):
+    if (a < 0 or b < 0):
+        return
+    
+    x = a
+    y = b 
+    while (y != 0):
+        r = x % y
+        x = y
+        y = r
+        
+    return x
+
+
+
+gcd1 = input("Enter your first value: ") 
+integ1 = int(gcd1)
+gcd2 = input("Entes yous second value: ")
+integ2 = int(gcd2)
+mega = EU(integ1, integ2) 
+print(mega)
